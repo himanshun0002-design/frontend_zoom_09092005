b6c131cdf2f0811457251b78ed2e7df2c4de5994
himanshu chauhan <himanshun00002@gmail.com>
2025-09-15 18:58:12 +0530

    Prepare React project for Render deployment

--- zoom_clone/chat/routing.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import consumers
-
-websocket_urlpatterns = [
-    path("ws/chat/", consumers.ChatConsumer.as_asgi()),
-    path("ws/video/<str:room_name>/", consumers.VideoConsumer.as_asgi()),
-]
